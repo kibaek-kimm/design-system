2b778aef8c194255f987736e1339351b52d61a11
Kibaek Kim <kibaek.kimm@gmail.com>
2021-01-07 16:34:52 +0900

    fix: Button 컴포넌트 수정

---
+++ packages/dsc-button/component.tsx
@@ -0,0 +1,5 @@
+import React, { useEffect } from "react"
+
+export const Button = () => (
+    <button>Button</button>
+)
--- packages/dsc-button/lib/dsc.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = dsc;
-
-function dsc() {
-    // TODO
-}
