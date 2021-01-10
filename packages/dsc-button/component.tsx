51c8251ec92fbdd4a9b21f0db2e764f02e55eb3a
Kibaek Kim <kibaek.kimm@gmail.com>
2021-01-11 00:32:08 +0900

    feat: button 컴포넌트 타입수정

--- packages/dsc-button/component.tsx
+++ packages/dsc-button/component.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react"
+import { IButtonProps } from "./types"
 
-export const Button = () => (
-    <button>Button</button>
-)
+export const Button: React.FC<IButtonProps> = ({ buttonType = "default" }) => {
+  const cn = buttonType;
+  return <button className={cn}>Button</button>
+}
---
+++ packages/dsc-button/types/index.d.ts
@@ -0,0 +1,5 @@
+export type ButtonType = "primary" | "secondary" | "error" | "default";
+
+export interface IButtonProps {
+  buttonType: ButtonType
+}
